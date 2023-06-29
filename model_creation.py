bd4b761f10b433de0ce94732d521eae13d2320e1
Jacob Zigby <jibz1998@gmail.com>
2023-06-29 17:28:49 -0400

    Started loading data
    Going to create a function to load and split the data for the model

--- model_creation.py
+++ model_creation.py
@@ -1,5 +1,26 @@
 #This script will focus on creating the model
 #Will start with a keras convid2d model and attempt to make a model using pytorch later
-import tensorflow as tf
 
-print(tf.__version__)
+#Imports here
+import tensorflow as tf
+import numpy as np
+
+def main():
+    #Declare constants
+    DATA_PATH = "Data_Gathering\\Data\\De_Dup\\"
+
+    #Load cleaned data
+    print("Loading data...")
+    X = np.load(f"{DATA_PATH}X\\X.npy")
+    Y = np.load(f"{DATA_PATH}Y\\Y.npy")
+
+
+#function to load data
+def load_data(path):
+    pass
+
+if __name__ == "__main__":
+    print("Code started")
+    main()
+    print("Code executed successfully")
+    print("Big brain success!")
