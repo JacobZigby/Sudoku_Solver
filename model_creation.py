62d266bd15a6f24dc9fbef8622bdee7320e8fb5b
Jacob <jibz1998@gmail.com>
2023-06-29 15:08:31 -0400

    Adding Script
    Added script so I can pull

---
+++ model_creation.py
@@ -0,0 +1,5 @@
+#This script will focus on creating the model
+#Will start with a keras convid2d model and attempt to make a model using pytorch later
+import tensorflow as tf
+
+print(tf.__version__)
