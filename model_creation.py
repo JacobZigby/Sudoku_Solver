8ad7acaa92605ee02c7c8a9e8184ac643eb083b9
Jacob Zigby <jibz1998@gmail.com>
2023-07-09 20:08:08 -0400

    Updating model_creation file
    Updating file so I can do a pull on my main pc and keep working from where I left off... Basically I was testing stuff with floor division

--- model_creation.py
+++ model_creation.py
@@ -6,7 +6,9 @@ import tensorflow as tf
 import numpy as np
 
 def main():
+    #this is all just a test for now
     #Declare constants
+    #Path to cleaned data
     DATA_PATH = "Data_Gathering\\Data\\De_Dup\\"
 
     #Load cleaned data
@@ -14,9 +16,34 @@ def main():
     X = np.load(f"{DATA_PATH}X\\X.npy")
     Y = np.load(f"{DATA_PATH}Y\\Y.npy")
 
+    #basic split of 60/20/20
+    temp = len(X)
+    print(temp)
+    print(f"{temp*0.6}: 60%")
+    print(f"{temp*0.2}: 20%")
+    print(f"{temp*0.8}: 80%")
+    print(f"{(temp*0.6)+(temp*0.2)}: 60% + 20%")
+    print()
+    print(f"{temp//5}: 20%")
+    print(f"{temp//5*3}: 60%")
+    print(f"{temp//5*4}: 80%")
+    print()
+    #this one is the formula, get the 1% value, then multiply to the percentage you want
+    print(f"{temp//100*80}: 80%")
+
+
 
 #function to load data
 def load_data(path):
+    pass
+
+#function to split data into (train/test/validation) sets
+def data_split(x, y, percentages = (60,20,20)):
+    #percentage is a tuple and should equal 100 in the end, create validator to make sure this is true
+    #Check if percentage tuple adds up too 100
+    if(sum(list(percentages)) != 100):
+        #raise an error here
+        print("ERROR")
     pass
 
 if __name__ == "__main__":
