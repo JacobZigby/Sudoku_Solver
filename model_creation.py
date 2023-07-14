20d467017ee8b5357069fe015a35946e7d96807c
Jacob Zigby <jibz1998@gmail.com>
2023-07-14 02:10:31 -0400

    data_split function complete
    Finished the data_split method, it can now successefully shuffle data and split it according to parameters

--- model_creation.py
+++ model_creation.py
@@ -16,18 +16,25 @@ def main():
     X = np.load(f"{DATA_PATH}X\\X.npy")
     Y = np.load(f"{DATA_PATH}Y\\Y.npy")
 
+    #check to see that both files are of the same size
+    if len(X) != len(Y):
+        raise ValueError(f"X ({len(X)}) and Y ({len(Y)}) are of unequal length")
+    
     #basic split of 80/10/10
     temp = len(X)
     #this one is the formula, get the 1% value, then multiply to the percentage you want
     print(f"{temp//100*80}: 80%")
 
-    print("Testing X and Y value type:")
-    print(type(X[0]))
+    print("Testing X and Y values:")
+    print(X[0])
     print(Y[0])
 
     print("Test of data_split method")
-    data_split(X,Y, shuffle=True)
+    xs,ys= data_split(X,Y, shuffle=True)
+    print(xs[0][0])
+    print(ys[0][0])
 
+#come back to fix percentages later
 #function to split data into (train/test/validation) sets
 def data_split(x, y, percentages = (80,10,10), shuffle = False):
     #Function will return a nested tuple where the first layer will be for x, y and the next for train,test,validate
@@ -40,13 +47,16 @@ def data_split(x, y, percentages = (80,10,10), shuffle = False):
         #zip x and y values and do a basic
         temp = list(zip(x,y))
         random.shuffle(temp)
-        #unzip and reassing back to x and y
+        #unzip and reassigning back to x and y
         x,y = zip(*temp)
 
-        print("Test to see how the shuffle effects the values")
-        print(type(x[0]))
-        print(y[0])
+    #get index location for each split
+    data_size = len(x)
+    q1 = data_size//100*percentages[0]
+    q2 = (data_size//100*percentages[1]) +  q1
 
+    #making each split here, and storing into tuples
+    return (x[:q1],x[q1:q2],x[q2:]),(y[:q1],y[q1:q2],y[q2:])
 
 
 
